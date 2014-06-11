333554f9f70185c02203897e884e4c0d8323abca
Brandon Nguyen <bnguyen9469@yahoo.com>
2014-06-10 20:31:15 -0700

    Did menu and started css. Added webfonts & jquery.

---
+++ header.php
@@ -0,0 +1,37 @@
+<link href="styles/headers.css" rel="stylesheet">
+<link href="styles/main.css" rel="stylesheet">
+
+<div id="nav">
+    <ul>
+            <li>
+            <a href="/">
+                <img src="images/placeholder.png" width="50" height="50">
+                <br />
+                Home
+            </a>
+            </li>
+            <li>
+            <a href="">
+                <img src="images/placeholder.png" width="50" height="50">
+                <br />
+                Menu Item
+            </a>
+            </li>
+            
+            <li>
+            <a href="">
+                <img src="images/placeholder.png" width="50" height="50">
+                <br />
+                Menu Item
+            </a>
+            </li>
+            
+            <li>
+            <a href="">
+                <img src="images/placeholder.png" width="50" height="50">
+                <br />
+                Menu Item
+            </a>
+            </li>
+    </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Class of 2019</title>
+</head>
+<?php include "header.php" ?>
+<body>
+	
+</body>
+</html>
