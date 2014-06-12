77a715a45187a1108c1211a3079afbfccc56ae05
bnguyen654 <bnguyen9469@yahoo.com>
2014-06-11 23:53:03 -0700

    Workin on submenus, doin some CSS.

--- header.php
+++ header.php
@@ -1,35 +1,48 @@
 <link href="styles/headers.css" rel="stylesheet">
 <link href="styles/main.css" rel="stylesheet">
+<script src="config/jquery.js"></script>
 
 <div id="nav">
     <ul>
             <li>
             <a href="/">
-                <img src="images/placeholder.png" width="50" height="50">
-                <br />
+                <img src="images/placeholder.png">
                 Home
             </a>
             </li>
             <li>
             <a href="">
-                <img src="images/placeholder.png" width="50" height="50">
-                <br />
+                <img src="images/placeholder.png">
+                Menu w/ Submenu
+            </a>
+      <ul class="submenu">
+            	<li>
+                <a href="">
+                <img src="images/placeholder.png">
+                Submenu Item
+                </a>
+                </li>
+                
+				<li>
+                <a href="">
+                <img src="images/placeholder.png">
+                Submenu Item
+                </a>
+                </li>
+
+            </ul>
+            </li>
+            
+            <li>
+            <a href="">
+                <img src="images/placeholder.png">
                 Menu Item
             </a>
             </li>
             
             <li>
             <a href="">
-                <img src="images/placeholder.png" width="50" height="50">
-                <br />
-                Menu Item
-            </a>
-            </li>
-            
-            <li>
-            <a href="">
-                <img src="images/placeholder.png" width="50" height="50">
-                <br />
+                <img src="images/placeholder.png">
                 Menu Item
             </a>
             </li>
