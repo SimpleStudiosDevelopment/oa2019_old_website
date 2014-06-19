552e6ed7331f6d79912838fb4ae90f34acfbf963
bnguyen654 <bnguyen9469@yahoo.com>
2014-06-19 00:33:05 -0700

    Admin panel for restructuring added. Working on CSS.

--- restructuring/adm/index.php
+++ restructuring/adm/index.php
@@ -42,39 +42,145 @@ foreach($sets as $val){
 }
 ?>
 
+<title>Restructuring Admin Info</title>
 
 <h1>Restructuring: Admin Panel</h1>
 
-<div>
-
-<div class="sector-head"><h3>Section Title</h3></div>
-
-<div class="sector-a">
-<h5 style="display:inline;">Status:</h5>
-<p style="display:inline; color:green;">Open</p>
-
-<h5 style="display:inline;">Requests:</h5>
-<p style="display:inline;"> <?php $data['new_admin']?> </p>
+<div class="section">
+    <div class="sector-head">
+        <h2>Bad Rule</h2>
+    </div>
+    <div class="sector-a">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:green;">Open</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo $data['bad_rule']?></p>
+    </div>
+    <a class="nouline" href="https://docs.google.com/spreadsheets/d/1H3Y-lq9ViKozlUP09DsmbjAbPiHFlOAxRDfpBssyc9Y/edit?usp=sharing" target="_blank">
+    <div class="sector-a">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="https://docs.google.com/forms/d/1kpBKJ0-b6oEcehUwfw_iLT2VWKepZIdFQ73ZWjr8q4U/viewform?usp=send_form" target="_blank">
+    <div class="sector-a sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
 </div>
 
-<div class="sector-a"><p>See Responses (You must be admin)</p></div>
-<div class="sector-a sector-foot">See Form</div>
-
+<div class="section">
+    <div class="sector-head">
+        <h2>New Rule</h2>
+    </div>
+    <div class="sector-b">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:green;">Open</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo $data['new_rule']?></p>
+    </div>
+    <a class="nouline" href="https://docs.google.com/spreadsheets/d/1FeWmXVGDCSyXGAS7YPPaN-MJLDq5IxSkkFYhtCDh3p8/edit?usp=sharing" target="_blank">
+    <div class="sector-b">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="https://docs.google.com/forms/d/1I7LAKvVntFAHG5TMsEyg5WLzy_O8uNdwumQeU_AnY68/viewform?usp=send_form" target="_blank">
+    <div class="sector-b sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
 </div>
 
-<div>
-
-<div class="sector-head"><h3>Section Title</h3></div>
-
-<div class="sector-b">
-<h5 style="display:inline;">Status:</h5>
-<p style="display:inline; color:green;">Open</p>
-
-<h5 style="display:inline;">Requests:</h5>
-<p style="display:inline;"> <?php $data['new_admin']?> </p>
+<div class="section">
+    <div class="sector-head">
+        <h2>Bad Admin</h2>
+    </div>
+    <div class="sector-a">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:green;">Open</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo $data['bad_admin']?></p>
+    </div>
+    <a class="nouline" href="https://docs.google.com/spreadsheets/d/1MhBrptrZaHDEQ7vhDj4OHFPN3DE49lsDgRIX6fmWgh4/edit?usp=sharing" target="_blank">
+    <div class="sector-a">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="https://docs.google.com/forms/d/1rTJHMmHHUSmxu6-w0K6_iuSsjdr1VYUv2elWjnP16TQ/viewform?usp=send_form" target="_blank">
+    <div class="sector-a sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
 </div>
 
-<div class="sector-b"><p>See Responses (You must be admin)</p></div>
-<div class="sector-b sector-foot">See Form</div>
-
+<div class="section">
+    <div class="sector-head">
+        <h2>Request to be Admin</h2>
+    </div>
+    <div class="sector-b">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:green;">Open</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo $data['new_admin']?></p>
+    </div>
+    <a class="nouline" href="https://docs.google.com/spreadsheets/d/1uKIdwY-ITGavmIsmgs3BjGNIOR6CRoWpLnGua-dxMVQ/edit?usp=sharing" target="_blank">
+    <div class="sector-b">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="https://docs.google.com/forms/d/1_sc07-sIeJ9vNhZUa_kukk0lGgMU7L0n_uyH-x9yBYY/viewform?usp=send_form" target="_blank">
+    <div class="sector-b sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
 </div>
+
+<div class="section">
+    <div class="sector-head">
+        <h2>Admin Voting</h2>
+    </div>
+    <div class="sector-a">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:#FC0;">Unopened</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo 'N/A'?></p>
+    </div>
+    <a class="nouline" href="#">
+    <div class="sector-a">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="#">
+    <div class="sector-a sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
+</div>
+
+<div class="section">
+    <div class="sector-head">
+        <h2>Misc Suggestions</h2>
+    </div>
+    <div class="sector-b">
+    	<h4 style="display:inline;">Status:</h4>
+        <p style="display:inline; color:green;">Open</p>
+        <br />
+        <h4 style="display:inline;">Requests:</h4>
+        <p style="display:inline;"><?php echo $data['misc']?></p>
+    </div>
+    <a class="nouline" href="https://docs.google.com/spreadsheets/d/1dj-cvKOmIlLkyKzZYl-ePW7FwnnqoZFwn4XGvfrC7UY/edit?usp=sharing" target="_blank">
+    <div class="sector-b">
+        <h4>Responses</h4>
+    </div>
+    </a>
+    <a class="nouline" href="https://docs.google.com/forms/d/1jjLfyka-aE95UFLvsq_BLPpm51_n2n_zZOuv4c8B4do/viewform?usp=send_form" target="_blank">
+    <div class="sector-b sector-foot">
+        <h4>Form</h4>
+    </div>
+    </a>
+</div>
+
